66206ba73ef819c0caa1afbda5fc515653c5d24e
Waleed Khan <arxanas@gmail.com>
2014-09-06 18:21:34 -0400

    Added Parse, Facebook APIs.

---
+++ README.md
@@ -0,0 +1,8 @@
+Installation
+------------
+
+  1) `pip install -r requirements.txt`.
+  2) `bower install`
+  3) Add a file called `parse_api_keys.js` going by the example in
+     `parse_api_keys.example`.
+  4) Add a file called `init_fb.js` as in `init_fb.js.example`.
--- dinner/dinner.py
+++ dinner/dinner.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask
 import sys
 
 app = Flask(__name__, static_url_path="")
@@ -7,6 +7,16 @@ app = Flask(__name__, static_url_path="")
 @app.route("/")
 def home():
     return app.send_static_file("index.html")
+
+
+@app.route("/parse_api_keys.js")
+def parse_api_keys():
+    return open("parse_api_keys.js").read()
+
+
+@app.route("/init_fb.js")
+def init_fb():
+    return open("init_fb.js").read()
 
 if __name__ == "__main__":
     debug = "debug" in sys.argv
